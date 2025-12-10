d1f366c0ac02a78672d1e8d4031b1e1a13e642a4
danilabond899-commits <danilabond899@gmail.com>
2025-12-11 00:08:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
 
+document.getElementById("btn1").addEventListener("click", () => {
+    tg.sendData(JSON.stringify({
+        name: "Dior Sauvage",
+        price: "5200 ₽"
+    }));
+    tg.close();
+});
+
+document.getElementById("btn2").addEventListener("click", () => {
+    tg.sendData(JSON.stringify({
+        name: "Chanel Bleu",
+        price: "6100 ₽"
+    }));
+    tg.close();
+});
